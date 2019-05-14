0678ea79ce6934ec133dc1b472601f7ae7596d95
Marco Koch <marco@5th.ch>
2019-05-14 23:54:11 +0100

    most basic unstyled theme ever.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+    <main>
+        <section>
+
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<h2>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h2>
+                <?the_content(); ?>
+
+			</article>
+
+            <?php endwhile; else : ?>
+                <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
+
+        </section>
+
+    </main>
+
