64b78fbf67e9fb294a90fa08202384d7306533ab
Marco Koch <marco@5th.ch>
2019-05-16 23:21:42 +0100

    minimalist 404 page.

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+    <main>
+        <section>
+
+			<article id="post-404">
+
+				<h2>
+					404
+				</h2>
+
+                <p>page not found.</p>
+
+			</article>
+
+        </section>
+
+    </main>
+</body>
+</html>
